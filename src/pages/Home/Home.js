977af9802fac974b99038ec0a78490f5e3b7d994
Dylan L <dylancblarocque@gmail.com>
2023-02-10 14:10:58 -0700

    Add Bio to Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,113 +1,26 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Header from "../../components/Header/Header";
-import TransactionCard from "../../components/TransactionCard/TransactionCard";
+import picture from "../../assets/photos/6.jpeg"
+import "./Home.scss"
 
 function Home(){
-    const [difficulty, setDifficulty] = useState(null);
-    const [btcPrice, setBtcPrice] = useState(null);
-    const [dataLoaded, setDataLoaded] = useState(false);
-    const [walletDetails, setWalletDetails] = useState({});
-    const [walletLoaded, setWalletLoaded] = useState(false);
-    const [tx, setTx] = useState([]);
-    const [txLoaded, setTxLoaded] = useState(false);
-
-    // get bitcoin price
-    const getBtcPrice = () => {
-      const btcEndpoint = "https://api.coinbase.com/v2/prices/BTC-CAD/spot";
-      axios.get(btcEndpoint)
-        .then(({ data }) => {
-          setBtcPrice(data);
-          setDataLoaded(true);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
-
-    // get difficulty adjustment
-    const getDifficulty = async () => {
-      const miningEnpoint = "https://mempool.space/api/v1/difficulty-adjustment";
-      try {
-        const {data} = await axios.get(miningEnpoint);
-        setDifficulty(data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    const getWalletDetails = async () => {
-      const backend = "http://localhost:8080/wallet";
-      try {
-        const {data} = await axios.get(backend);
-        setWalletDetails(data);
-        setWalletLoaded(true);
-      } catch(error){
-        console.log(error);
-      }
-    }
-
-    async function getData(url, apikey, content_type) {
-      let headers = {};
-      if (apikey) {
-        headers["X-Api-Key"] = apikey;
-      }
-      if (content_type) {
-        headers["Content-Type"] = content_type;
-      }
     
-      const response = await axios.get(url, { headers });
-      return response.data;
-    }
-
-    async function getLnbitsTransactions() {
-      let data = await getData(`https://legend.lnbits.com/api/v1/payments`, 
-      `0dc7444ff5fc446aa53949f96adb7dc1`,
-      `application/json` );
-      setTx(data);
-      setTxLoaded(true);
-      console.log(data);
-      console.log(tx);
-      return data;
-    }
-      
-    useEffect(() => {
-      getBtcPrice();
-      getDifficulty();
-      getWalletDetails();
-      getLnbitsTransactions();
-    }, []);
-    
-    if (!dataLoaded && !walletLoaded && !txLoaded) return <div>Loading...</div>;
-
-    const txItem = tx.map((x) => (
-      <TransactionCard 
-        key={x.checking_id}
-        x={x}
-      />
-    ));
-
-    const dollarValue = (btcPrice.data.amount) * (walletDetails.balance) / 1000000000;
-
-
-  
     return (
       <>
-      <Header />
-        <h1> BTC Price</h1>
-        <h1>${btcPrice.data.amount}</h1>
-
-        <h2>Wallet Name: {walletDetails.name}</h2>
-        <div><h4> Wallet Balance:</h4>
-        <h5>{walletDetails.balance} </h5> 
-        <h5> $ {dollarValue} Canadian</h5>
-        </div>
-        
-       <div>
-         {txItem}
-
-       </div> 
-
+       <main className="bio">
+        <section className="bio__picture-section">
+          <img className="bio__picture" src={picture} alt="ubermensch"></img>
+          <div className="bio__links">
+            <a href="https://www.linkedin.com/in/dylan-larocque-/">Link</a>
+            <a href="https://github.com/larocquedylan">Git</a>
+            <a href="https://www.twitch.tv/dylphur">twitch</a>
+          </div>
+          
+        </section>
+        <section className="bio__details-section">
+          <p> I am Dylan LaRocque. My aim is focussed on securing property rights for Canada'a First Nations peoples through the use of the Bitcoin Protocol. Which I believe ot be the most interesting human rights tool we have. </p>
+          <p> I believe that unless the rules of physics render something impossible, that all problems have a solution and require  the search for good explanations. </p>
+          <p> </p>
+        </section>
+       </main>
       </>
     )
   };
