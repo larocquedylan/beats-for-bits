51ba48c0e54f1fefd8dd87d07e79ba5c430a57a7
Dylan L <dylancblarocque@gmail.com>
2023-02-10 14:11:22 -0700

    Add Wallet Route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import SongsPage from './pages/SongsPage/SongsPage';
 import Header from './components/Header/Header';
+import WalletPage from './pages/WalletPage/WalletPage';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/songs' element={<SongsPage />} />
+        <Route path='/wallet' element={<WalletPage />} />
       </Routes>
     </Router>
     </>
