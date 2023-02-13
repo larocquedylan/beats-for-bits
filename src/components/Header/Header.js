0c58a35227506f8394ba551fcfe7696141bdd312
Dylan L <dylancblarocque@gmail.com>
2023-02-12 21:39:23 -0700

    Tablet Header Finished. Desktop almost

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -26,6 +26,12 @@ function Header(){
 
     //   const styles = {{transform: isMobileNav ? 'translateX(100%)' : 'translate(50%)' }};
 
+    // const backgroundValue = isOpen ? '../../assets/icons/Bitcoin-Icons-0.1.7/svg/filled/menu.svg' : '../../assets/icons/Bitcoin-Icons-0.1.7/svg/filled/cross.svg';
+
+    // const toggleBtnStyles = {
+    //   'background-image': `url(${backgroundValue})`
+    // };
+
     return (
         <>
         <section className="header">
@@ -36,7 +42,8 @@ function Header(){
                     <h1 className="header__title"> Dylan LaRocque</h1>
                 </Link>
                 <div className="header__nav-fill" > </div>
-                <button className="mobile-nav-toggle display" onClick={toggleOpen}> </button>
+                {/* <button className="mobile-nav-toggle display" onClick={toggleOpen} style={toggleBtnStyles}> </button> */}
+                <button className="mobile-nav-toggle display" onClick={toggleOpen} > </button>
                 {/* <nav className="header__nav-links" style={translateNav}>  */}
                 <nav className="header__nav-links"> 
                     <ul className="nav-bar" style={{ transform: isOpen ? 'translateX(0%)' : 'translate(-100%)' }}> 
