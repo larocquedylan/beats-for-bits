869c6946693a3a822828ca3aa52d00139abc59e5
Dylan L <dylancblarocque@gmail.com>
2023-02-13 10:25:56 -0700

    Bio Page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom";
 import picture from "../../assets/photos/6.jpeg"
+import Resume from "../../components/Resume/Resume";
+import visible from "../../assets/icons/Bitcoin-Icons-0.1.7/svg/filled/visible.svg"
 import "./Home.scss"
 
 function Home(){
@@ -16,11 +19,22 @@ function Home(){
           
         </section>
         <section className="bio__details-section">
-          <p> I am Dylan LaRocque. My aim is focussed on securing property rights for Canada'a First Nations peoples through the use of the Bitcoin Protocol. Which I believe ot be the most interesting human rights tool we have. </p>
+          <p> I'm a developer. I create offerings and try to bring value to the teams and people around me with strategy and curiosity. </p>
+            
+            <p>My aim is focussed on securing property rights for Canada'a First Nations peoples through the use of the Bitcoin Protocol. Which I believe to be the most compelling human rights and decolonial tool we have. </p>
+
           <p> I believe that unless the rules of physics render something impossible, that all problems have a solution and require  the search for good explanations. </p>
-          <p> </p>
+
+          <p> In the past, I have been a UX Designer with BC Public Service, a Marketing Analyst @ OneFeather, as well as a Research Assistant in the Marketing &amp; Behavioural Science Group at Robert H Lee Graduate School. </p>
+
+          {/* <p> I also make noises with my computer and sell them for Bitcoin over the Lightning Network which you can check out <Link to={`/songs`} className="bio__link">Here <img src={visible} alt="arrow tag"></img></Link> */}
+          <p> I also make noises with my computer and sell them for Bitcoin over the Lightning Network which you can check out <Link to={`/songs`} className="bio__link">Here</Link>
+          </p>
         </section>
-       </main>
+        </main>
+
+        <Resume />
+       
       </>
     )
   };
---
+++ src/pages/SongPage/SongPage.js
@@ -0,0 +1,11 @@
+import "./SongPage.scss"
+
+function SongPage(){
+    return (
+        <>
+        
+        </>
+    );
+};
+
+export default SongPage;
