153ea88eb0afd2d57235613bbbbb69937aa1d45c
Dylan L <dylancblarocque@gmail.com>
2023-02-13 23:48:55 -0700

    Header complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,50 +1,29 @@
 import "./Header.scss"
 import lightning from "../../assets/icons/Bitcoin-Icons-0.1.7/svg/filled/lightning.svg"
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useState } from "react";
 
 
 function Header(){
-    // const [translate, setTranslate] = useState("20%");
-    // const [count, setCount ] = useState(1);
-
     const [isOpen, setOpen] = useState(false);
 
     const toggleOpen = () => {
     setOpen(!isOpen);
     };  
 
-    // const toggleMobileNav = () => {
-    //     setCount(count + 1);
-    //     console.log(count);
-
-    // }
-
-    // const translateNav = {
-    //     transform: `translateX(`{mobileNav}`)`
-    //   };
-
-    //   const styles = {{transform: isMobileNav ? 'translateX(100%)' : 'translate(50%)' }};
-
-    // const backgroundValue = isOpen ? '../../assets/icons/Bitcoin-Icons-0.1.7/svg/filled/menu.svg' : '../../assets/icons/Bitcoin-Icons-0.1.7/svg/filled/cross.svg';
-
-    // const toggleBtnStyles = {
-    //   'background-image': `url(${backgroundValue})`
-    // };
 
     return (
         <>
         <section className="header">
-            <div className="header__left-fill"> </div>
+
             <div className="header__nav"> 
                 <Link to={'/'} className="header__title-wrapper"> 
                     <img src={lightning} className="header__img" alt="lightning"></img>
-                    <h1 className="header__title"> Dylan LaRocque</h1>
+                    <h1 className="header__title"> Beats For Bits</h1>
                 </Link>
-                <div className="header__nav-fill" > </div>
-                {/* <button className="mobile-nav-toggle display" onClick={toggleOpen} style={toggleBtnStyles}> </button> */}
-                <button className="mobile-nav-toggle display" onClick={toggleOpen} > </button>
-                {/* <nav className="header__nav-links" style={translateNav}>  */}
+
+                <button className="mobile-nav-toggle-btn" onClick={toggleOpen} > </button>
+  
                 <nav className="header__nav-links"> 
                     <ul className="nav-bar" style={{ transform: isOpen ? 'translateX(0%)' : 'translate(-100%)' }}> 
                         <Link to={`/songs`} className="header__link">Music</Link>
@@ -53,7 +32,6 @@ function Header(){
                     </ul>
                 </nav>
             </div>
-            <div className="header__right-fill"> </div>
         </section>
         </>
     );
