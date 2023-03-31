4166e7e76085038a2cfedd2ccc94caccbdff2cf8
Dylan L <dylancblarocque@gmail.com>
2023-03-31 13:03:11 -0700

    setStream in useEffect

--- src/components/Audio/Audio.js
+++ src/components/Audio/Audio.js
@@ -1,23 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Audio.scss';
 import AudioPlayer from 'react-h5-audio-player';
 
 const Audio = ({ song, songId }) => {
+  const [streamUrl, setStreamUrl] = useState(null);
+
   useEffect(() => {
-    console.log('song', song);
-  }, [song]);
+    // console.log('song', song);
+    setStreamUrl(`http://localhost:8080/songs/stream/${songId}`);
+  }, [songId]);
 
   // Set the source to the streaming endpoint
-  const streamUrl = `http://localhost:8080/songs/stream/${songId}`;
+  // const streamUrl = `http://localhost:8080/songs/stream/${songId}`;
 
   return (
     <div className=''>
       <h2>Audio</h2>
-      <AudioPlayer
-        src={streamUrl}
-        volume={0.5}
-        // Try other props!
-      />
+      <AudioPlayer src={streamUrl} volume={0.5} />
     </div>
   );
 };
