263e7f03a247118032f47c77a4045e1a1b60af58
Dylan L <dylancblarocque@gmail.com>
2023-03-30 22:50:59 -0700

    streaming enabled on client + backend

--- src/components/Audio/Audio.js
+++ src/components/Audio/Audio.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Audio.scss';
 import AudioPlayer from 'react-h5-audio-player';
 
-const Audio = ({song}) => {
+const Audio = ({ song, songId }) => {
+  useEffect(() => {
+    console.log('song', song);
+  }, [song]);
+
+  // Set the source to the streaming endpoint
+  const streamUrl = `http://localhost:8080/songs/stream/${songId}`;
+
   return (
     <div className=''>
-        <h2>Audio</h2>
-        <AudioPlayer
-          src={song.audio}
-          volume={0.5}
-          // Try other props!
-        />
+      <h2>Audio</h2>
+      <AudioPlayer
+        src={streamUrl}
+        volume={0.5}
+        // Try other props!
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Audio;
